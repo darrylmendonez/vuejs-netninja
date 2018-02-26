61af44e1dcdfc059999c7c22a11300b65e912b37
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2018-02-25 19:33:25 -0500

    finished lesson 19 on nesting and importing components

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p>{{ greeting() }}</p>
+    <ninjas></ninjas>
   </div>
 </template>
 
 <script>
+import Ninjas from './Ninjas.vue'
 export default {
+  components: {
+    'ninjas': Ninjas
+  },
   data () {
     return {
-      title: 'Your first Vue file, wooo!'
-    }
-  },
-  methods: {
-    greeting: function() {
-      return 'heeey cowboy!';
+      title: 'Ninja App'
     }
   }
 }
