51aa66ed88c40ef1a66fbbbf70af6a5222100311
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2018-03-02 18:16:57 -0500

    finished lesson 43 on hooking up to firebase part 1

--- vuejs-playlist/src/components/addBlog.vue
+++ vuejs-playlist/src/components/addBlog.vue
@@ -55,11 +55,7 @@
     }, // /data
     methods: {
       post: function() {
-        this.$http.post('http://jsonplaceholder.typicode.com/posts', {
-          title: this.blog.title,
-          body: this.blog.content,
-          userId: 1
-        }).then(function(data) {
+        this.$http.post('https://synaptik-digital-shared-notes.firebaseio.com/posts.json', this.blog).then(function(data) {
           console.log(data);
           this.submitted = true;
         });
